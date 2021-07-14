2823134097f315f792be96994137a90aa9a86dd8
mattnedgus <mmsugden@gmail.com>
2021-07-14 22:54:36 +0100

    Initial Re-Commit After Making Structs

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,31 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    addAndMakeVisible(toggleButton);
+    setSize (600, 400);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setFont (juce::Font (48.0f));
+    g.setColour (juce::Colours::red);
+    g.drawText ("Hello New PFM C++ World!", getLocalBounds(), juce::Justification::centred, true);
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+    toggleButton.setBounds(10, 10, 30, 30);
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+struct Car
+{
+    struct CarSeat
+    {
+        bool seatIsLeather;
+    };
+    int numberOfWheels;
+    bool convertible;
+    CarSeat driverSeat;
+    CarSeat navigatorSeat;
+
+    bool switchSeats(CarSeat oldseat, CarSeat newSeat);
+
+    void accelerate(float howFarToPushThePedal);
+};
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent  : public juce::Component
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+    juce::ToggleButton toggleButton;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
